9451f6363bb57a52f4f0abc4dfc1316068e15d46
bbilgin01 <baturalp.bilgin@ozu.edu.tr>
2026-03-29 19:05:29 +0300

    hello-to-mambo

---
+++ dependency-checker.c
@@ -0,0 +1,30 @@
+#ifdef PLUGINS_NEW
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+#include "../plugins.h"
+
+__attribute__((constructor)) void dependency_checker_init(void) {
+
+    mambo_context *ctx = mambo_register_plugin();
+    mambo_register_pre_thread_cb(ctx, dependency_checker_pre_thread);
+    mambo_register_post_thread_cb(ctx, dependency_checker_post_thread);
+}
+
+int dependency_checker_pre_thread(mambo_context *ctx){
+    fprintf(stderr, "dependency_checker: pre thread callback\n");
+    return 0;   
+}
+
+int dependency_checker_post_thread(mambo_context *ctx){
+    fprintf(stderr, "dependency_checker: post thread callback\n");
+    return 0;
+}
+
+
+
+#endif /* PLUGINS_NEW */
